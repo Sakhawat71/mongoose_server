d8a8534f1581d1ccae60731f5a49a726495c6174
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2024-11-17 17:55:24 +0600

    mongooose and dotenv

--- dist/server.js
+++ dist/server.js
@@ -1,10 +1,33 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
 const app_1 = __importDefault(require("./app"));
-const PORT = 5000;
-app_1.default.listen(PORT, () => {
-    console.log(`Example app listening on PORT ${PORT}`);
-});
+const config_1 = __importDefault(require("./app/config"));
+// const PORT = 5000;
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield mongoose_1.default.connect(config_1.default.db_url);
+            app_1.default.listen(config_1.default.port, () => {
+                console.log(`Example app listening on PORT ${config_1.default.port}`);
+            });
+        }
+        catch (error) {
+            console.error("Error connecting to the database:", error);
+            process.exit(1); // Exit the process with a failure code
+        }
+    });
+}
+main();
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,23 @@
-import app from "./app"
 import mongoose from "mongoose";
-const PORT = 5000;
+import app from "./app"
+import config from "./app/config";
+// const PORT = 5000;
 
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-    
+    try {
+        await mongoose.connect(config.db_url as string);
+
+
+        app.listen(config.port, () => {
+            console.log(`Example app listening on PORT ${config.port}`)
+        })
+
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+        process.exit(1); // Exit the process with a failure code
+    }
 }
 
-app.listen(PORT, () => {
-    console.log(`Example app listening on PORT ${PORT}`)
-})
+main()
