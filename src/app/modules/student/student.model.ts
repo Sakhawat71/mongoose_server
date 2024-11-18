e376e897c81cfb3f3fda0d97171f9057e80c424b
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2024-11-18 12:14:22 +0600

    module -> interface -> model

---
+++ src/app/modules/student/student.model.ts
@@ -0,0 +1,55 @@
+import { Schema } from 'mongoose';
+import { IStudent, IAddress, ICourses } from './student.interface';
+
+
+const addressSchema = new Schema<IAddress>({
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    postalCode: { type: String, required: true },
+    country: { type: String, required: true }
+})
+
+const courseSchema = new Schema<ICourses>({
+    courseId: { type: String, required: true },
+    courseName: { type: String, required: true },
+    instructor: { type: String, required: true },
+    credits: { type: Number, required: true },
+    grade: { type: String } // Optional field
+});
+
+
+export const studentSchema = new Schema<IStudent>({
+    id: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    dateOfBirth: { type: String, required: true },
+    gender: { type: String, enum: ['Male', 'Female', 'Other'], required: true },
+    email: { type: String, required: true },
+    phoneNumber: { type: String },
+
+    address: { type: addressSchema, require: true },
+
+    enrollmentDate: { type: String, required: true },
+    graduationDate: { type: String },
+
+    courses: [courseSchema],
+
+    gpa: { type: Number },
+    major: { type: String, required: true },
+    minor: { type: String },
+    extracurricularActivities: { type: [String] },
+    scholarships: [{
+        name: { type: String, required: true },
+        amount: { type: Number, required: true },
+        awardedDate: { type: String, required: true }
+    }],
+    attendanceRecords: [{
+        date: { type: String, required: true },
+        status: { type: String, enum: ['Present', 'Absent', 'Late'], required: true }
+    }],
+    profilePictureUrl: { type: String },
+    notes: { type: [String] },
+    createdAt: { type: String, required: true },
+    updatedAt: { type: String, required: true },
+});
